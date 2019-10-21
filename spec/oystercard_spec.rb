c7a2914de3f63ce5fdffeb9e6a6622fc7569e9fe
Andy Jones <andytjones@icloud.com>
2019-10-21 14:54:40 +0100

    Challange 5 complete. Top-up functioning.

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,9 +1,18 @@
 require "oystercard"
 
 describe Oystercard do
-  
+  subject(:card) { Oystercard.new }
+
   it 'initialise oystercard with balance of 0' do
-    test_card = Oystercard.new
-    expect(test_card.balance).to eq 0
+    expect(card.balance).to eq 0
   end
+
+  describe 'top_up' do
+
+    it 'can top up the balance' do
+      expect{  card.top_up 10 }.to change{ card.balance }.by 10
+    end
+
+  end
+
 end
