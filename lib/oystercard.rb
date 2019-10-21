e1ba885e3bba258f96b24962896641c159c2a6de
Alistair Phipps <alistair.phipps@johnlewis.co.uk>
2019-10-21 15:08:57 +0100

    challenge 6 done

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,11 +1,14 @@
 class Oystercard
   attr_reader :balance
+  LIMIT = 90
 
   def initialize
     @balance = 0
   end
 
   def top_up(value)
+    raise "top up limit #{LIMIT} reached" if @balance + value > LIMIT
+
     @balance += value
   end
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -7,12 +7,15 @@ describe Oystercard do
     expect(card.balance).to eq 0
   end
 
-  describe 'top_up' do
+  describe '#top_up' do
 
     it 'can top up the balance' do
       expect{  card.top_up 10 }.to change{ card.balance }.by 10
     end
 
+    it 'throws error when trying to top up past limit' do
+      expect { card.top_up 100 }.to raise_error(RuntimeError, "top up limit #{Oystercard::LIMIT} reached")
+    end
   end
 
 end
