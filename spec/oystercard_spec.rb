3d5e634eb9c048c6b8496ad68bf2d8c815fbae4c
Andy Jones <andytjones@icloud.com>
2019-10-21 14:32:59 +0100

    Oystercard balance 0 test passed.

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -2,4 +2,8 @@ require "oystercard"
 
 describe Oystercard do
   
+  it 'initialise oystercard with balance of 0' do
+    test_card = Oystercard.new
+    expect(test_card.balance).to eq 0
+  end
 end
